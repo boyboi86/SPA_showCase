101912411b3fe980293390d7b087dfbb198a00eb
boyboi86 <boyboi86@gmail.com>
2016-06-19 12:14:40 +0800

    added files and components

---
+++ script/app.js
@@ -0,0 +1,48 @@
+var myApp = angular.module('myApp', ['ui.bootstrap', 'ngRoute']);
+
+//Routes
+myApp.config(function($routeProvider){
+    
+    $routeProvider
+    
+    .when('/',{
+        
+        templateUrl:'pages/home.htm',
+        controller:'mainCtrl'
+        
+    })
+    
+    .when('/contact',{
+        
+        templateUrl:'pages/contact.htm',
+        controller:'contactCtrl'
+        
+    })
+    
+});
+
+// Controllers
+myApp.controller('mainCtrl', ['$scope', function($scope){
+    
+    
+    
+}]);
+
+myApp.controller('contactCtrl', ['$scope', function($scope){
+    
+    
+    
+}]);
+
+// Carousel Controller
+myApp.controller('CarouselCtrl', CarouselCtrl);
+ function CarouselCtrl($scope) {
+  $scope.myInterval = 2000;
+  $scope.slides = [
+      {image:'assets/img/image01.jpg',text:'Image1'},
+      {image:'assets/img/image02.jpg',text:'Image2'},
+      {image:'assets/img/image03.jpg',text:'Image3'},
+      {image:'assets/img/image05.jpg',text:'Image4'}];
+  
+    var slides = $scope.slides;
+};
